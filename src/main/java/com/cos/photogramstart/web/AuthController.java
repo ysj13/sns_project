fc9e8c711987da2c03888966d58ba2df15409636
sj <ysj13@kakao.com>
2022-12-30 04:18:54 +0900

    signup

---
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -0,0 +1,24 @@
+package com.cos.photogramstart.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/auth/signin")
+    public String signinForm() {
+        return "auth/signin";
+    }
+
+    @GetMapping("/auth/signup")
+    public String signupForm() {
+        return "auth/signup";
+    }
+
+    @PostMapping("/auth/signup")
+    public String signup() {
+        return "auth/signin";
+    }
+}
