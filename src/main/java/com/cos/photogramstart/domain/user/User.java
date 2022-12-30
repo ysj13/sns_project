55ef24ba192f65d06ef9a6dc68bd271e94d58aa4
sj <ysj13@kakao.com>
2022-12-31 05:30:13 +0900

    비밀번호 암호화

--- src/main/java/com/cos/photogramstart/domain/user/User.java
+++ src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -19,7 +19,9 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String username;
+
     private String password;
     private String email;
     private String name;
--- src/main/java/com/cos/photogramstart/service/AuthService.java
+++ src/main/java/com/cos/photogramstart/service/AuthService.java
@@ -3,15 +3,23 @@ package com.cos.photogramstart.service;
 import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
+    @Transactional
     public User 회원가입(User user) {
+        String rawPassword = user.getPassword();
+        String encPassword = passwordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        user.setRole("ROLE_USER");
         User userEntity = userRepository.save(user);
         return userEntity;
     }
