f88cc6436def58c8d06c8a776f768553e47cceb1
sj <ysj13@kakao.com>
2023-01-03 15:23:24 +0900

    예외처리 공통 컨트롤러

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -1,18 +1,17 @@
 package com.cos.photogramstart.handler;
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.web.dto.CustomResponseDto;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(CustomValidationException.class)
-    public Map<String, String> validationException(CustomValidationException e) {
-        return e.getErrorMap();
+    public CustomResponseDto<?> validationException(CustomValidationException e) {
+        return new CustomResponseDto(-1, e.getMessage(), e.getErrorMap());
     }
 }
