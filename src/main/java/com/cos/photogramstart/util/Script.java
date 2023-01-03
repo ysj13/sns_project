b3e4451f25a08aa536733b99f463ad605aae72f1
sj <ysj13@kakao.com>
2023-01-04 06:08:59 +0900

    예외처리 알림후 되돌아가기

---
+++ src/main/java/com/cos/photogramstart/util/Script.java
@@ -0,0 +1,14 @@
+package com.cos.photogramstart.util;
+
+public class Script {
+
+    public static String back(String message) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("<script>");
+        sb.append("alert('" + message + "');");
+        sb.append("history.back();");
+        sb.append("</script>");
+
+        return sb.toString();
+    }
+}
