ef0e254c90e91f041978a355c0e3da2f700c8dec
sj <ysj13@kakao.com>
2023-02-08 16:20:33 +0900

    로거삭제

--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -4,8 +4,6 @@ import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.service.AuthService;
 import com.cos.photogramstart.web.dto.auth.SignupDto;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +14,6 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 @Controller
 public class AuthController {
-
-    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
 
     private final AuthService authService;
 
